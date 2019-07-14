133e1ea85fed9e8a988be0c8333d1b31982d9ba9
Phil Guo <phil.g@nyu.edu>
2019-07-14 02:33:04 -0700

    Fixup D3D12

--- Samples/Raytracing/HelloDXR/Data/HelloDXR.rt.hlsl
+++ Samples/Raytracing/HelloDXR/Data/HelloDXR.rt.hlsl
@@ -25,8 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***************************************************************************/
-RWTexture2D<float4> gOutput;
-
+shared RWTexture2D<float4> gOutput;
 __import Raytracing;
 
 shared cbuffer PerFrameCB
